67c952c3e04824fa451bf31923587a3301eddace
waqar-ahmad356 <flashdrivepunjab@gmail.com>
2024-02-15 13:42:06 +0500

    add post and delete api

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.get('/get',(req,resp)=>{
     
 });
 
-app.post('/update',(req,resp)=>{
+app.post('/insert',(req,resp)=>{
     const data={
-        name:'waqar ahmad',price:1200,category:"mobile",brand:"iphone"
+        name:'ayesha siddiqa',price:1200,category:"mobile",brand:"iphone"
     }
     conn.query("insert into user set ?",data,(err,result,fields)=>{
         if(err)
@@ -31,5 +31,33 @@ app.post('/update',(req,resp)=>{
             resp.send("updated data")
         }
     })
+})
+app.put('/update',(req,resp)=>{
+    const data=['haider','1200','phone','samsung','23112'];
+    conn.query('update user set name=?,price=?,category=?,brand=? where price=?',data,(err,result,fields)=>{
+        if (err)
+        {
+            console.log('Error occured');
+        }
+        else
+        {
+            resp.send(result);
+        }
+
+    })
+    resp.send('updated');
+})
+app.delete('/delete',(req,resp)=>{
+    const data=['waqar'];
+    conn.query('delete from users where name=?',data,(err,result,fields)=>{
+        if (err)
+        {
+            console.log('error occured');
+        }
+        else{
+            resp.send(result);
+        }
+    })
+    resp.send('deleted successfully!');
 })
 app.listen(4500);
